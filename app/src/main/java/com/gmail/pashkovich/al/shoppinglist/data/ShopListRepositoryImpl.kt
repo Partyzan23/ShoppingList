22d5e797decbcac796ef835405a2056e93586a90
Alex Pashkovich <al.pashkovich@gmail.com>
2023-02-16 12:25:04 +0100

    Override Repository functions

--- app/src/main/java/com/gmail/pashkovich/al/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/gmail/pashkovich/al/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package com.gmail.pashkovich.al.shoppinglist.data
 
-object ShopListRepositoryImpl {
+import com.gmail.pashkovich.al.shoppinglist.domain.ShopItem
+import com.gmail.pashkovich.al.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    override fun addShopItem(shopItem: ShopItem) {
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
 }
