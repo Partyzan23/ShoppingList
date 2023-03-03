d4f00a844cac21090e7695c7ca5621947f95bdd6
Alex Pashkovich <al.pashkovich@gmail.com>
2023-03-03 13:54:34 +0100

    Revision getItemViewType

--- app/src/main/java/com/gmail/pashkovich/al/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/gmail/pashkovich/al/shoppinglist/presentation/ShopListAdapter.kt
@@ -11,8 +11,8 @@ import com.gmail.pashkovich.al.shoppinglist.domain.ShopItem
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
     companion object{
-        private const val ITEM_TYPE_ENABLED = 100
-        private const val ITEM_TYPE_DISABLED = 101
+        private const val VIEW_TYPE_ENABLED = 100
+        private const val VIEW_TYPE_DISABLED = 101
     }
 
     var shopList = listOf<ShopItem>()
@@ -22,10 +22,10 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val layoutResId = if (viewType == ITEM_TYPE_ENABLED) {
-            R.layout.item_shop_enabled
-        } else {
-            R.layout.item_shop_disabled
+        val layoutResId = when(viewType){
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type $viewType")
         }
         val view = LayoutInflater.from(parent.context).inflate(
             layoutResId,
@@ -47,9 +47,9 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     override fun getItemViewType(position: Int): Int {
         val shopItem = shopList[position]
         return if (shopItem.enabled) {
-            ITEM_TYPE_ENABLED
+            VIEW_TYPE_ENABLED
         } else {
-            ITEM_TYPE_DISABLED
+            VIEW_TYPE_DISABLED
         }
     }
 
